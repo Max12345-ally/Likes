875f0cf4eb93930909a9c507371801cc439f0219
maxbond <ceo@rf.ru>
2023-02-15 23:58:05 +0300

    First init

---
+++ src/components/LikePhotoApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Title from "./Title";
+
+function LikePhotoApp() {
+  return (
+    <div className="container text-center">
+      <Title text={"Like photo app"} />
+      <Title classes={"subtitle "} text={"Likes"} />
+      <div className="card card-dark m-auto"></div>
+      .card
+    </div>
+  );
+}
+
+export default LikePhotoApp;
